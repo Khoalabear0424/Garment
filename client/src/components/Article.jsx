96cf7ff12762449af30e3dd54f5b67b5fa7699ab
Khoa Nguyen <37357006+Khoalabear0424@users.noreply.github.com>
2019-05-08 21:59:13 -0700

    deleted some un needed elements

--- client/src/components/Article.jsx
+++ client/src/components/Article.jsx
@@ -9,7 +9,6 @@ class Article extends Component {
         const { name, imgSrc, link, prev, curr, discount } = this.props
 
         return (
-            // <div className="container  m-4" data-id="1">
             <figure className="figure article m-4">
                 <img src={imgSrc} className="figure-img img-fluid rounded" alt="" />
                 <figcaption className="figure-caption">
@@ -22,7 +21,6 @@ class Article extends Component {
                     <span className="percentOff"> {discount} </span>
                 </figcaption>
             </figure>
-            // </ div>
         )
     }
 }
