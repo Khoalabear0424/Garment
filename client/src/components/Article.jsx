8bd25cdfd7671f7698b24747f9eb802ee6eb9943
Khoa Nguyen <37357006+Khoalabear0424@users.noreply.github.com>
2019-05-08 20:09:53 -0700

    created article component and imported it into app.js

---
+++ client/src/components/Article.jsx
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+
+class Article extends Component {
+    state = {}
+    render() {
+        return (<h1>Hello</h1>);
+    }
+}
+
+export default Article;
