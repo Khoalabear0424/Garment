7b1108d3e7345dfd7f9474dbf21cea7024f51bbf
Khoa Nguyen <37357006+Khoalabear0424@users.noreply.github.com>
2019-05-08 20:39:45 -0700

    used figure from bootstrap to display article item

--- client/src/components/Article.jsx
+++ client/src/components/Article.jsx
@@ -7,26 +7,21 @@ class Article extends Component {
     render() {
         return (
             <div className="container article m-4" data-id="1">
-                <div className="jumbotron" >
-                    <img className="itemImage" src="https://lp2.hm.com/hmgoepprod?set=source[/87/e0/87e0c05fc42ade2e753e6da605be865a6755b80b.jpg],origin[dam],category[ladies_knitwear_turtlenecks],type[LOOKBOOK],res[m],res[s],hmver[1]&call=url[file:/product/main]" alt="Image Item" />
-                    <a href="<%= data[i].link %>">
-                        <h5>Item Name</h5>
-                    </a>
-                    <p className="priceDisplay"><span className="originalPrice">
-                        $12.00</span><span className="price">
+                <figure class="figure">
+                    <img src="https://lp2.hm.com/hmgoepprod?set=source[/87/e0/87e0c05fc42ade2e753e6da605be865a6755b80b.jpg],origin[dam],category[ladies_knitwear_turtlenecks],type[LOOKBOOK],res[m],res[s],hmver[1]&call=url[file:/product/main]" class="figure-img img-fluid rounded" alt="A generic square placeholder image with rounded corners in a figure." />
+                    <figcaption class="figure-caption">
+                        <a href="<%= data[i].link %>">
+                            <h5>Item Name</h5>
+                        </a>
+                        <span className="originalPrice">
+                            $12.00</span><span className="price">
                             $10.00 </span> <span className="percentOff">
                             15%</span> <button className="btn-success btn btn-sm" data-id="<1>">Save</button>
-                    </p>
+                    </figcaption>
+                </figure>
+            </ div>
+        )
+    }
+}
 
-                    <figure class="figure">
-                        <img src="..." class="figure-img img-fluid rounded" alt="A generic square placeholder image with rounded corners in a figure.">
-                            <figcaption class="figure-caption">A caption for the above image.</figcaption>
-                    </figure>
-                    
-                </ div>
-                </ div>
-                )
-            }
-        }
-        
 export default Article;
