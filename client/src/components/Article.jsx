785f7fd02b9269fc5dd2e9357758e25110744eda
Khoa Nguyen <37357006+Khoalabear0424@users.noreply.github.com>
2019-05-08 20:59:35 -0700

    added heart liked icon to article

--- client/src/components/Article.jsx
+++ client/src/components/Article.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import '../App.css';
+import Like from "./common/like";
 
 class Article extends Component {
     state = {}
@@ -10,13 +11,13 @@ class Article extends Component {
                 <figure class="figure">
                     <img src="https://lp2.hm.com/hmgoepprod?set=source[/87/e0/87e0c05fc42ade2e753e6da605be865a6755b80b.jpg],origin[dam],category[ladies_knitwear_turtlenecks],type[LOOKBOOK],res[m],res[s],hmver[1]&call=url[file:/product/main]" class="figure-img img-fluid rounded" alt="A generic square placeholder image with rounded corners in a figure." />
                     <figcaption class="figure-caption">
-                        <a href="<%= data[i].link %>">
-                            <h5 className="itemName">Item Name</h5>
-                        </a>
+                        <h6>
+                            <a href="<%= data[i].link %>" className="itemName">Item Name  </a>
+                            <Like />
+                        </h6>
                         <span className="originalPrice">$12.00</span>
                         <span className="price">  $10.00 </span>
                         <span className="percentOff"> 15% </span>
-                        <button className="btn-success btn btn-sm" data-id="<1>">Save</button>
                     </figcaption>
                 </figure>
             </ div>
