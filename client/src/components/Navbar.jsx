38e8acc3702491ad87bf60b10b5b34e06c8e5685
Khoa Nguyen <37357006+Khoalabear0424@users.noreply.github.com>
2019-05-09 20:44:07 -0700

    changed navbar color to pink for bae

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -5,7 +5,7 @@ class Navbar extends Component {
     state = {}
     render() {
         return (
-            <nav className="navbar navbar-expand-lg navbar-light bg-light">
+            <nav className="navbar navbar-expand-lg navbar-light" style={{ "background-color": "#FFB6C1" }}>
                 <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
