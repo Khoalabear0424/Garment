3fdb08e3f7285e88c8c60f184e5d111b42d6b974
Khoa Nguyen <37357006+Khoalabear0424@users.noreply.github.com>
2019-05-19 22:19:59 -0700

    refactored scrape nordstrom into scrape folder

---
+++ routes/scrape/nordstrom.js
@@ -0,0 +1,100 @@
+const express = require('express');
+const app = express();
+const router = express.Router();
+const puppeteer = require('puppeteer');
+const cheerio = require('cheerio');
+var mongojs = require("mongojs");
+
+
+var databaseUrl = "garmet_DB";
+var collections = ["scrapedData", "savedItems"];
+var db = mongojs(databaseUrl, collections);
+db.on("error", function (error) {
+    console.log("Database Error:", error);
+});
+
+router.get('/', function (req, res) {
+    async function scrape() {
+        function wait(ms) {
+            return new Promise(resolve => setTimeout(() => resolve(), ms));
+        }
+
+        const browser = await puppeteer.launch({ headless: false, defaultViewport: null });
+        const page = await browser.newPage();
+        await page.goto('https://shop.nordstrom.com/c/all-womens-sale');
+
+        // Get the height of the rendered page
+        let bodyHandle = await page.$('body');
+        let { height } = await bodyHandle.boundingBox();
+        await bodyHandle.dispose();
+
+        // Scroll one viewport at a time, pausing to let content load
+        const viewportHeight = 500
+        let viewportIncr = 0;
+        let pages = 0;
+
+        while (pages < 2) {
+            while (viewportIncr + viewportHeight < 27500) {
+                await page.evaluate(_viewportHeight => {
+                    window.scrollBy(0, 300);
+                }, viewportHeight);
+                await wait(50);
+                viewportIncr = viewportIncr + viewportHeight;
+            }
+            let content = await page.content();
+            var $ = cheerio.load(content);
+            var data = []
+            $('article').each(function (i, elem) {
+                data[i] =
+                    {
+                        name: $($($(this))).find('h3').children().children().text(),
+                        brand: "Nordstrom",
+                        brandLogo: "https://upload.wikimedia.org/wikipedia/commons/thumb/1/1f/Nordstrom_Logo.svg/1280px-Nordstrom_Logo.svg.png",
+                        src: $(this).find('div').find('img').attr('src'),
+                        link: 'https://shop.nordstrom.com' + $(this).find('a').attr('href'),
+                        price: {
+                            prev: $($($(this))).find('div').eq(-2).children().last().text().split(" ")[0],
+                            curr: $($($(this))).find('div').eq(-1).children().eq(-2).text().split(" ")[0],
+                            discount: $($($(this))).find('div').eq(-1).children().last().html()
+                        }
+
+                    }
+            })
+            insertDataIntoDB(data)
+            await wait(3000);
+            await page.click('.nui-icon-large-chevron-right');
+            viewportIncr = 0
+            bodyHandle = await page.$('body');
+            pages++
+        }
+        res.send(data)
+        await browser.close();
+    };
+
+    function insertDataIntoDB(value) {
+        for (let i in value) {
+            db.scrapedData.insert({
+                name: value[i].name,
+                brand: value[i].brand,
+                brandLogo: value[i].brandLogo,
+                src: value[i].src,
+                link: value[i].link,
+                price: {
+                    prev: value[i].price.prev,
+                    curr: value[i].price.curr,
+                    discount: value[i].price.discount
+                }
+            }, function (error, newItem) {
+                if (error) {
+                    console.log(error)
+                } else {
+                    console.log(`Added item ${value[i].name}`);
+                }
+            })
+        }
+    }
+
+    scrape()
+})
+
+module.exports = router;
