9940b6b086f29044375a275ffb8e213c789e69c1
Khoa Nguyen <37357006+Khoalabear0424@users.noreply.github.com>
2019-05-23 17:36:55 -0700

    added type to each item

--- routes/api/sortWordType.js
+++ routes/api/sortWordType.js
@@ -13,18 +13,41 @@ db.on("error", function (error) {
 router.get('/', function (req, res) {
     db.nameTracker.find({
         'word': {
-            $nin: ["(Nordstrom", "Exclusive)", "Size)", "Burch", "Tory", "Midi", "Front", "Plus", "Leith", "Jeans", "Navy", "Something", "Gibson", "Slide", "Skinny", "Waist", "High", "BP.", "J", "Wash", "Lace", "Sam", "Flat", "Edelman", "Ruffle", "Sleeve", "Eliza", "Crossbody", "Neck", "Topshop", "Wrap", "Halogen®", "Shoulder", "Linen", "Small", "Caslon®", "Ankle", "Blend", "the", "Rebecca", "Maxi", "Wide-Leg", "Mule", "Espadrille", "Button", "Minkoff", "Print", "Vince", "Society", "Emmett", "Long", "Toe", "Zella", "Faux", "UGG®", "Strap", "Blouse", "Camuto", "Relaxed", "Eco", "Rigid", "Petite", "Ruched", "Convertible", "Off", "Pump", "Sugarplum!", "Hi", "Camisole", "Minidress", "(Plus", "Women's", "International", "Day", "MARC", "JACOBS", "Button-Front", "High-Rise", "Mid-Rise", "9\"", "Short", "A-Line", "Slim", "Embroidered", "Knit", "Tunic", "Nordstrom", "Mesh", "All", "Halter", "Shift", "Sheath", "Crepe", "Easy", "(Women)", "&", "in", "(Regular", "Petite)", "The", "x", "Madewell", "Mini", "Edition", "Whisper", "Treasure", "Bond", "Bardot", "MCM", "Side", "Square", "Pocket", "Texture", "Thread", "Classic", "Scalloped", "Central", "Cami", "Silk", "Straw", "Bralette", "Swing", "Ribbed", "Tall", "Prairie", "Jansing", "Straight", "Stretch", "Pointy", "Back", "Sole", "by", "Leg", "BLANKNYC", "Weekend", "Fleming", "Chloé", "Favor", "Suede", "Lulus", "Pleat", "Platform", "Detail", "Knot", "Henley", "Fit", "Flip", "Flare", "Burberry", "Pleated", "Sleeveless", "Ravello", "Watch,", "Flap", "Double", "Split", "Bucket", "Canvas", "Metallic", "Pack", "Baker", "London", "Ted", "Wallet", "Nylon", "Corsage", "Boardwalk", "Vintage", "Rosalie", "Woven", "Summer", "Ruffle-Strap", "Ryder", "Pouch", "Confetti", "Lace-Up", "Crisscross", "Ruthie", "Bandana", "Delmar", "One-Piece", "Popover", "Chelsea", "Posies", "Socks", "Tie-Front", "Girls", "Graphic", "Women", "Inc.", "Muscle", "Windowpane", "Cheetah", "Rainbow", "Charles", "Smocked", "Henry", "ASTR", "Body-Con", "Label", "Rosi", "Distressed", "Signature", "Hazel", "7/8", "In", "Live", "Soft", "Jersey", "+", "Model", "Make", "Driving", "Madden", "Steve", "Low", "Boyfriend", "Wide", "Mary", "Faye", "Chiffon", "Peplum", "Heel", "Hinge", "Drape", "Row", "A", "Articles", "Ripped", "of", "Belted", "Everie", "(Regular,", "Quilted", "Trench", "Hem", "Cozy", "Colorblock", "Longchamp", "NYDJ", "Pullover", "MICHELE", "Cap", "Bateau", "38mm", "Everyday", "Off-White", "a", "Givenchy", "Wash:"]
+            $nin: ["(Nordstrom", "Exclusive)", "Size)", "Burch", "Tory", "Midi", "Front", "Plus", "Leith", "Jeans", "Navy", "Something", "Gibson", "Slide", "Skinny", "Waist", "High", "BP.", "J", "Wash", "Lace", "Sam", "Flat", "Edelman", "Ruffle", "Sleeve", "Eliza", "Crossbody", "Neck", "Topshop", "Wrap", "Halogen®", "Shoulder", "Linen", "Small", "Caslon®", "Ankle", "Blend", "the", "Rebecca", "Maxi", "Wide-Leg", "Mule", "Espadrille", "Button", "Minkoff", "Print", "Vince", "Society", "Emmett", "Long", "Toe", "Zella", "Faux", "UGG®", "Strap", "Blouse", "Camuto", "Relaxed", "Eco", "Rigid", "Petite", "Ruched", "Convertible", "Off", "Pump", "Sugarplum!", "Hi", "Camisole", "Minidress", "(Plus", "Women's", "International", "Day", "MARC", "JACOBS", "Button-Front", "High-Rise", "Mid-Rise", "9\"", "Short", "A-Line", "Slim", "Embroidered", "Knit", "Tunic", "Nordstrom", "Mesh", "All", "Halter", "Shift", "Sheath", "Crepe", "Easy", "(Women)", "&", "in", "(Regular", "Petite)", "The", "x", "Madewell", "Mini", "Edition", "Whisper", "Treasure", "Bond", "Bardot", "MCM", "Side", "Square", "Pocket", "Texture", "Thread", "Classic", "Scalloped", "Central", "Cami", "Silk", "Straw", "Bralette", "Swing", "Ribbed", "Tall", "Prairie", "Jansing", "Straight", "Stretch", "Pointy", "Back", "Sole", "by", "Leg", "BLANKNYC", "Weekend", "Fleming", "Chloé", "Favor", "Suede", "Lulus", "Pleat", "Platform", "Detail", "Knot", "Henley", "Fit", "Flip", "Flare", "Burberry", "Pleated", "Sleeveless", "Ravello", "Watch,", "Flap", "Double", "Split", "Bucket", "Canvas", "Metallic", "Pack", "Baker", "London", "Ted", "Wallet", "Nylon", "Corsage", "Boardwalk", "Vintage", "Rosalie", "Woven", "Summer", "Ruffle-Strap", "Ryder", "Pouch", "Confetti", "Lace-Up", "Crisscross", "Ruthie", "Bandana", "Delmar", "One-Piece", "Popover", "Chelsea", "Posies", "Socks", "Tie-Front", "Girls", "Graphic", "Women", "Inc.", "Muscle", "Windowpane", "Cheetah", "Rainbow", "Charles", "Smocked", "Henry", "ASTR", "Body-Con", "Label", "Rosi", "Distressed", "Signature", "Hazel", "7/8", "In", "Live", "Soft", "Jersey", "+", "Model", "Make", "Driving", "Madden", "Steve", "Low", "Boyfriend", "Wide", "Mary", "Faye", "Chiffon", "Peplum", "Heel", "Hinge", "Drape", "Row", "A", "Articles", "Ripped", "of", "Belted", "Everie", "(Regular,", "Quilted", "Trench", "Hem", "Cozy", "Colorblock", "Longchamp", "NYDJ", "Pullover", "MICHELE", "Cap", "Bateau", "38mm", "Everyday", "Off-White", "a", "Givenchy", "Wash:", "Denim", "Tie"]
         }
-    }).sort({ 'count': -1 }).limit(45, function (error, found) {
+    }).sort({ 'count': -1 }).limit(100, function (error, found) {
         if (error) {
             console.log(error);
         }
         else {
-            var wordArr = []
+            var wordArr = {}
             for (let i in found) {
-                wordArr.push(found[i].word)
+                wordArr[found[i].word] = found[i].word
             }
-            res.json(wordArr)
+            wordArr["Jeans"] = "Jeans";
+            wordArr["MiniDress"] = "MiniDress";
+            // res.json(wordArr)
+
+            db.scrapedData.find({}, function (error, found) {
+                if (error) {
+                    console.log(error);
+                }
+                else {
+                    for (let i in found) {
+                        var itemName = found[i].name.split(" ");
+                        console.log(`itemName : ${itemName}`)
+                        for (let j in itemName) {
+                            if (wordArr[itemName[j]]) {
+                                console.log(`match: ${itemName[j]}`)
+                                db.scrapedData.update({ '_id': found[i]._id },
+                                    { '$set': { 'type': itemName[j] } })
+                                break;
+                            }
+                        }
+                    }
+                    res.json(found)
+                }
+            });
         }
     });
 })
