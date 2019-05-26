f3696257a7124e29c63b1b6d01a51fd3b25776e0
Khoa Nguyen <37357006+Khoalabear0424@users.noreply.github.com>
2019-05-26 16:46:24 -0700

    formatted nextbuton

--- client/src/components/common/pagination.jsx
+++ client/src/components/common/pagination.jsx
@@ -5,18 +5,39 @@ const Pagination = props => {
     const { itemsCount, pageSize, onPageChange, currentPage } = props;
     const pageCount = Math.ceil(itemsCount / pageSize);
     if (pageCount === 1) return null;
-    const pages = _.range(1, pageCount + 1);
+    const currentRange = Math.ceil(currentPage / 5)
+    const pages = _.range(currentRange * 5 - 4, currentRange * 5 + 1);
+
+    const nextButton = () => {
+        if (currentPage === pageCount) return null
+        return <li
+            className="pagination-item"
+            onClick={() => onPageChange(currentPage + 1)}>
+            >>
+        </li>
+    }
 
     return (
-        <ul className="pagination justify-content-center">
-            {pages.map((page, index) =>
-                <li
-                    key={index}
-                    className={page === currentPage ? 'selected-link pagination-item' : 'pagination-item'}
-                    onClick={() => onPageChange(page)}>
-                    {page}
-                </li>)}
-        </ul>
+        < ul className="pagination justify-content-center" >
+            {
+                pages.map((page, index) =>
+                    <li
+                        key={index}
+                        className={page === currentPage ? 'selected-link pagination-item' : 'pagination-item'}
+                        onClick={() => onPageChange(page)}>
+                        {page}
+                    </li>)
+            }
+            < li className="pagination-item" >...</li >
+
+            <li
+                className="pagination-item"
+                className={pageCount === currentPage ? 'selected-link pagination-item' : 'pagination-item'}
+                onClick={() => onPageChange(pageCount)}>
+                {pageCount}
+            </li>
+            {nextButton()}
+        </ul >
     )
 }
 
