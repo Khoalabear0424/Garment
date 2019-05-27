e664b534631a5b6228ce264cc19973cb65c8d1c7
Khoa Nguyen <37357006+Khoalabear0424@users.noreply.github.com>
2019-05-26 17:27:58 -0700

    fixed conditional pagination

--- client/src/components/common/pagination.jsx
+++ client/src/components/common/pagination.jsx
@@ -5,20 +5,48 @@ const Pagination = props => {
     const { itemsCount, pageSize, onPageChange, currentPage } = props;
     const pageCount = Math.ceil(itemsCount / pageSize);
     if (pageCount === 1) return null;
-    const currentRange = Math.ceil(currentPage / 5)
-    const pages = _.range(currentRange * 5 - 4, currentRange * 5 + 1);
+    const currentRange = Math.ceil(currentPage / 5);
+    const renderRange = (parseInt(pageCount - currentPage) === 0 ? currentPage : currentRange * 5 + 1);
+    const pages = _.range(currentRange * 5 - 4, renderRange);
 
     const nextButton = () => {
         if (currentPage === pageCount) return null
         return <li
             className="pagination-item"
             onClick={() => onPageChange(currentPage + 1)}>
-            >>
+            <i className="fa fa-angle-double-right" />
+        </li>
+    }
+
+    const backButton = () => {
+        if (currentRange === 1) return null
+        return <li
+            className="pagination-item"
+            onClick={() => onPageChange(currentPage - 1)}>
+            <i className="fa fa-angle-double-left"></i>
+        </li>
+    }
+
+    const elipses = () => {
+        if (parseInt(pageCount - currentPage) <= 5) return null
+        return <li
+            className="pagination-item no-hover">
+            ...
+        </li>
+    }
+
+    const lastPage = () => {
+        return <li
+            className="pagination-item-elipses"
+            className={pageCount === currentPage ? 'selected-link pagination-item' : 'pagination-item'}
+            onClick={() => onPageChange(pageCount)}>
+            {pageCount}
         </li>
     }
 
     return (
         < ul className="pagination justify-content-center" >
+            {backButton()}
             {
                 pages.map((page, index) =>
                     <li
@@ -28,14 +56,8 @@ const Pagination = props => {
                         {page}
                     </li>)
             }
-            < li className="pagination-item" >...</li >
-
-            <li
-                className="pagination-item"
-                className={pageCount === currentPage ? 'selected-link pagination-item' : 'pagination-item'}
-                onClick={() => onPageChange(pageCount)}>
-                {pageCount}
-            </li>
+            {elipses()}
+            {lastPage()}
             {nextButton()}
         </ul >
     )
