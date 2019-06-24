f3a98bdd8e70f18225b4826bf72e691a6ba7629c
Khoa Nguyen <37357006+Khoalabear0424@users.noreply.github.com>
2019-06-24 08:04:31 -0700

    manually fixed some jacket prices

--- client/src/components/Article.jsx
+++ client/src/components/Article.jsx
@@ -2,18 +2,30 @@ import React, { Component } from 'react';
 import '../App.css';
 import Like from "./common/like";
 
-const discountPercentDisplay = (discount, name) => {
+const discountPercentDisplay = (priceCurr, discount, name) => {
     console.log(`discount: ${discount}, itemName: ${name}, isFinite ${isFinite(discount)}`)
-    return discount ? '%off' : false;
+
+    return priceCurr ? discount + '%off' : false;
 }
 
-const dollarSignDisplay = (price) => {
+const dollarSignDisplayPrev = (price) => {
     let firstChar = price.slice(0, 1);
     if (firstChar !== "$") {
         return `$${price}`;
     }
     return price;
 }
+
+const dollarSignDisplayCurr = (price) => {
+    if (!price) return null;
+    return price ? "$" + price.toFixed(2) : "$" + price;
+}
+
+const priceFormatting = (pricePrev, priceCurr) => {
+    if (!priceCurr) return "price"
+    return "originalPrice"
+}
+
 
 class Article extends Component {
     render() {
@@ -27,9 +39,9 @@ class Article extends Component {
                         <a href={link} className="itemName">{name} </a>
                         {/* <Like /> */}
                     </h6>
-                    <span className="originalPrice">{dollarSignDisplay(prev)}</span>
-                    <span className="price"> ${curr ? curr.toFixed(2) : curr} </span>
-                    <span className="percentOff"> {discount}{discountPercentDisplay(discount, name)} </span>
+                    <span className={priceFormatting(prev, curr)}>{dollarSignDisplayPrev(prev)}</span>
+                    <span className="price"> {dollarSignDisplayCurr(curr)} </span>
+                    <span className="percentOff">{discountPercentDisplay(curr, discount, name)} </span>
                     <img className="figure-img img-fluid rounded brand" src={brandLogo} alt="brandLogo" />
                 </figcaption>
             </figure>
